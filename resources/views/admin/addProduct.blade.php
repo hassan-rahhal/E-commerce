7c9e1d8501fe247ee80f8bbf5900faa12c0a35a6
Hassan Rahhal <hassanrahhal09@gmail.com>
2025-06-22 00:48:12 +0200

    Adding new products

--- resources/views/admin/addProduct.blade.php
+++ resources/views/admin/addProduct.blade.php
@@ -5,6 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href='/assets/css/index.css' rel="stylesheet" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
     <script src="https://cdn.tiny.cloud/1/5i2hzh618cqamwy94m4f6jthwxyh3txe2pk6e6ueeo1596j2/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 <body>
